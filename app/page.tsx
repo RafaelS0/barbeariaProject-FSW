601c4d4a88d8ba59a342c2e8c80a100d58782e2b
Rafael <rafaeloliva2002@outlook.com>
2024-08-11 21:24:22 -0300

    feat: add scheduling card

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@
 import Image from "next/image"
 import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarFallback, AvatarImage } from "./_components/ui/avatar"
 import { Card, CardContent } from "./_components/ui/card"
 import { Input } from "./_components/ui/input"
 import { Search } from "lucide-react"
+
 
 const Home = () => {
   return <div>
@@ -21,12 +24,40 @@ const Home = () => {
   </div>
 
   <div className="relative w-full h-[150px]">
-    
   <Image alt="Agende nos melhores com fsw Barber" src="/Banner Pizza.png" fill className="object-contain rounded-xl"/> 
-
   </div>
 
+  <div>
+    <h2 className="font-semibold text-gray-400 p-3 ">AGENDAMENTOS</h2>
+  </div>
+  
+  <div>
+    <Card>
+     <CardContent className=" flex justify-between p-0 ">
+      {/*ESQUERDA*/}
+      <div className=" flex flex-col gap-2 py-5 pl-5">
+        <Badge className="w-fit">CONFIRMADO</Badge>
+         <h3>CORTE DE CABELO</h3>
+          <div className="flex items-center gap-2">
+           <Avatar className="h-6 w-6">
+            <AvatarImage src="https://github.com/shadcn.png"/>
+            <AvatarFallback>CN</AvatarFallback>
+           </Avatar>
+           <p className="text-sm">Barbearia FSW</p>  
+          </div>
+      </div>
+      
+     {/*DIREITA*/}
+      <div className="flex flex-col items-center justify-center px-5 border-l-2">
+        <p className="text-sm"> Agosto</p>
+        <p className="text-2xl">09</p>
+        <p className="text-sm">20:00</p>
+      </div>
+      </CardContent>
+    </Card>  
+  </div>
 
+ 
   </div>
 
 
